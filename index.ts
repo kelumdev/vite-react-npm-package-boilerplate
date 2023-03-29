d4358000597c9a1b502c81611e6d11602d8bf32f
Kelum Chathuranga <kelum.chathuranga@nzsafetyblackwoods.co.nz>
2023-03-29 15:25:53 +0530

    Added new component

--- index.ts
+++ index.ts
@@ -2,3 +2,4 @@ import './src/index.css'
 
 export { Button } from './src/components/Button'
 export { NewComp } from './src/components/NewComp'
+export { Kelum } from './src/components/Kelum'
---
+++ src/components/Kelum.tsx
@@ -0,0 +1,5 @@
+export const Kelum = (props: any) => {
+  return (
+    <div className=" bg-green-600 p-[50px] text-white">New one {props?.label}</div>
+  )
+}
